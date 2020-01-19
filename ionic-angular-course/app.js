622bf3eee25050fc461d06d56ddc5c9cbd61deef
Frank Edward Daza González <frank.daza2gmail.com>
2020-01-18 22:00:39 -0500

    Finishing Up the Base JavaScript Logic.

--- ionic-angular-course/app.js
+++ ionic-angular-course/app.js
@@ -1,8 +1,17 @@
 const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+
+let totalExpenses = 0;
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
 
 confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
@@ -16,4 +25,9 @@ confirmBtn.addEventListener('click', () => {
     newItem.textContent = enteredReason + ': $' + enteredAmount;
 
     expensesList.appendChild(newItem);
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
+    clear();
 });
+
+cancelBtn.addEventListener('click', clear);
