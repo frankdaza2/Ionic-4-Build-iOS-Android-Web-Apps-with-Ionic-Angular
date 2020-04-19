5658fcacefb2ca055e5f57a07b7b7ec01cbd4e30
Frank Edward Daza González <frank.daza2@gmail.com>
2020-04-19 15:48:10 -0500

    Implementing Virtual Scrolling

--- booking-routing/src/app/places/discover/discover.page.ts
+++ booking-routing/src/app/places/discover/discover.page.ts
@@ -10,11 +10,14 @@ import { PlacesService } from '../places.service';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
+
 
   constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.placesService.places.slice(1);
   }
 
 }
