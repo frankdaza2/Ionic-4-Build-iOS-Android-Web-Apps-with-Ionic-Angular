b14f2a3a7e039e15616ae0a280e9742f598f2328
Frank Edward Daza González <frank.daza2@gmail.com>
2020-04-24 21:32:40 -0500

    Using RxJS Subjects for State Management.

--- booking-routing/src/app/places/places.service.ts
+++ booking-routing/src/app/places/places.service.ts
@@ -1,47 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Place } from './place.model';
 import { AuthService } from '../auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
 
-  private _places: Place[] = [
-    new Place(
-      'p1',
-      'Manhattan Mansion',
-      'In the heart of New York City.',
-      'https://i.pinimg.com/originals/83/c6/bc/83c6bcc2981c5145965de0f2c1a395af.jpg',
-      149,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p2',
-      'San Francisco Apartment',
-      'In the heart of California.',
-      'https://static.trulia-cdn.com/pictures/thumbs_6/ps.88/d/4/2/8/picture-uh=3eee92c5cf2c3657bf837d3c3b87986-ps=d428e11a7e92dac8bf2633884f0395.jpg',
-      1234.67,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    ),
-    new Place(
-      'p3',
-      'Medellín Apartment',
-      'In the heart of Antioquia Colombia.',
-      'https://www.medellincolombia.co/wp-content/uploads/2018/03/Medellin-Apartment-Rental.jpg',
-      1000.58,
-      new Date('2019-01-01'),
-      new Date('2019-12-31'),
-      'abc'
-    )
-  ];
+  private _places = new BehaviorSubject<Place[]>(
+    [
+      new Place(
+        'p1',
+        'Manhattan Mansion',
+        'In the heart of New York City.',
+        'https://i.pinimg.com/originals/83/c6/bc/83c6bcc2981c5145965de0f2c1a395af.jpg',
+        149,
+        new Date('2019-01-01'),
+        new Date('2019-12-31'),
+        'abc'
+      ),
+      new Place(
+        'p2',
+        'San Francisco Apartment',
+        'In the heart of California.',
+        'https://static.trulia-cdn.com/pictures/thumbs_6/ps.88/d/4/2/8/picture-uh=3eee92c5cf2c3657bf837d3c3b87986-ps=d428e11a7e92dac8bf2633884f0395.jpg',
+        1234.67,
+        new Date('2019-01-01'),
+        new Date('2019-12-31'),
+        'abc'
+      ),
+      new Place(
+        'p3',
+        'Medellín Apartment',
+        'In the heart of Antioquia Colombia.',
+        'https://www.medellincolombia.co/wp-content/uploads/2018/03/Medellin-Apartment-Rental.jpg',
+        1000.58,
+        new Date('2019-01-01'),
+        new Date('2019-12-31'),
+        'abc'
+      )
+    ]
+  );
 
   get places() {
-    return [...this._places];
+    return this._places.asObservable();
   }
 
   constructor(private authService: AuthService) { }
@@ -61,9 +65,9 @@ export class PlacesService {
       dateTo,
       this.authService.userId
     );
-    this._places.push(newPlace);
-    console.log('places.service.ts -> addPlace -> ', newPlace);
-    console.log('places.service.ts -> _places -> ', this._places);
+    this.places.pipe(take(1)).subscribe(places => {
+      this._places.next(places.concat(newPlace));
+    });
     
   }
 }
