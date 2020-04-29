1750aba5f47d2e3ef61dfe28518439666df97eb9
Frank Edward Daza González <frank.daza2@gmail.com>
2020-04-28 22:25:45 -0500

    Adding the Google Maps SDK.

--- booking-routing/src/app/shared/map-modal/map-modal.component.ts
+++ booking-routing/src/app/shared/map-modal/map-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -6,13 +6,46 @@ import { ModalController } from '@ionic/angular';
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
+export class MapModalComponent implements OnInit, AfterViewInit {
 
   constructor(private modalController: ModalController) { }
 
   ngOnInit() {}
 
+  ngAfterViewInit(): void {
+    this.getGoogleMaps().then(googleMaps => {
+
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
   onCancel() {
     this.modalController.dismiss();
+  }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = 'https://www.google.com/maps/embed/v1/MODE?key=AIzaSyCfe4hdMmqynYKzTUVxHg2yaYrFLQDuPQk';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available.');
+        }
+      };
+    });
   }
 }
