24d849d5d51805d364c0e3cf3eff9504a246fbeb
Frank Edward Daza González <frank.daza2@gmail.com>
2020-04-28 23:25:33 -0500

    Finding the Address for a Place.

--- booking-routing/src/app/shared/pickers/location-picker/location-picker.component.ts
+++ booking-routing/src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-location-picker',
@@ -9,16 +12,35 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
 })
 export class LocationPickerComponent implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private httpClient: HttpClient) { }
 
   ngOnInit() {}
 
   onPickLocation() {
     this.modalController.create({ component: MapModalComponent }).then(modalEl => {
       modalEl.onDidDismiss().then(modalData => {
-        console.log(modalData.data);
+        if (!modalData.data) {
+          return;
+        }
+        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(address => {
+          console.log(address);
+          
+        });
       });
       modalEl.present();
     });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.httpClient
+      .get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`)
+        .pipe(map(geoData => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+
+          return geoData.results[0].formatted_address;
+        }));
+
   }
 }
--- booking-routing/src/environments/environment.prod.ts
+++ booking-routing/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  googleMapsAPIKey: 'AIzaSyCfe4hdMmqynYKzTUVxHg2yaYrFLQDuPQk'
 };
