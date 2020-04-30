a0140aabbc3a27f1fabbd7a21b8e19f0b85106c8
Frank Edward Daza González <frank.daza2@gmail.com>
2020-04-29 20:52:04 -0500

    Fetching a Static Image URL.

--- booking-routing/src/app/shared/pickers/location-picker/location-picker.component.ts
+++ booking-routing/src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -3,7 +3,9 @@ import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from 'src/app/places/location.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -22,9 +24,25 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(address => {
-          console.log(address);
-          
+
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null
+        };
+
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+          switchMap(address => {
+            pickedLocation.address = address;
+            return of(this.getMapImage(
+              pickedLocation.lat,
+              pickedLocation.lng,
+              14
+            ));
+          })
+        ).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
         });
       });
       modalEl.present();
@@ -41,6 +59,11 @@ export class LocationPickerComponent implements OnInit {
 
           return geoData.results[0].formatted_address;
         }));
+  }
 
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+      &markers=color:red%7Clabel:Place%7C${lat},${lng}
+      &key=${environment.googleMapsAPIKey}`;
   }
 }
